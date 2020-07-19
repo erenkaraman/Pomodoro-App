0ac969746234578908d7c9cfe709c4d2f4195985
Eren Karaman <erenkaraman58@hotmail.com>
2020-07-20 02:46:49 +0300

    version0.0.2

---
+++ appPomodoro/appPomodoro/appPomodoro/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace appPomodoro
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+            ToggleScreenLock();
+        }
+        public void ToggleScreenLock()
+        {
+            DeviceDisplay.KeepScreenOn = true;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
